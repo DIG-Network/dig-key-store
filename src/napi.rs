1d7c6705e74b9bd7dd0c8561c6bc9365b915a7d0
William Wills <wwills2@protonmail.com>
2025-12-22 12:35:48 -0500

    chore: clippy

--- src/napi.rs
+++ src/napi.rs
@@ -4,7 +4,6 @@ mod napi_impl {
     use napi_derive::napi;
     use std::sync::Arc;
     use std::time::Duration;
-    use tokio::runtime::Runtime;
 
     use crate::{Cache, CacheError, CacheOptions};
 
